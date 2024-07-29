e43df2bb30cdb470d082e39afa3f9a013dffe5e0
Casey Cochran <Casey3362@yahoo.com>
2024-07-28 18:26:14 -0700

    rock paper scissors refractor

--- index.js
+++ index.js
@@ -14,32 +14,80 @@ const getHumanChoice = () => {
 }
 
 const playRound = ( humanChoice, computerChoice ) => {
+
+    let playerScore = document.querySelector('.player-score');
+    let computerScore = document.querySelector('.computer-score');
+    let playerScoreNum = Number(document.querySelector('.player-score').textContent);
+    let computerScoreNum = Number(document.querySelector('.computer-score').textContent);
+
     const removeCasingChoice = humanChoice.toLowerCase();
+
     if ( removeCasingChoice === computerChoice ) {
-        return 'Its a tie';
+        return {
+            text: `It's a tie!`,
+            value: null
+        };
     } else if ( removeCasingChoice === 'rock' && computerChoice !== 'paper' ) {
-        humanScore++;
-        return `You win, rock beats ${computerChoice}`;
+        playerScore.textContent = ++playerScoreNum;
+        return {
+            text: `You win, paper beats ${computerChoice}`,
+            score: playerScoreNum,
+            player: 'human'
+        }
     } else if ( removeCasingChoice === 'paper' && computerChoice !== 'scissors' ) {
-        humanScore++;
-        return `You win, paper beats ${computerChoice}`;
+        playerScore.textContent = ++playerScoreNum;
+        return {
+            text: `You win, paper beats ${computerChoice}`,
+            score: playerScoreNum,
+            player: 'human'
+        }
     } else if ( removeCasingChoice === 'scissors' && computerChoice !== 'rock' ) {
-        humanScore++;
-        return `You win, scissors beats ${computerChoice}`;
+        playerScore.textContent = ++playerScoreNum;
+            return {
+            text: `You win, paper beats ${computerChoice}`,
+            score: playerScoreNum,
+            player: 'human'
+        }
     } else {
-        computerScore++;
-        return `You lose ${computerChoice} beats ${removeCasingChoice}`
+        computerScore.textContent = ++computerScoreNum;
+        return {
+            text: `You lose ${computerChoice} beats ${removeCasingChoice}`,
+            score: computerScoreNum,
+            player: 'computer'
+        }
     }
 }
 
+
+// Event listeners for choices. Displays who won the round.
+const buttons = document.querySelectorAll('button');
+ buttons.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        const playerChoice = e.target.value;
+        const roundWinnerObj = playRound(playerChoice, getComputerChoice());
+
+        let resultsDiv = document.querySelector('.results');
+        resultsDiv.textContent = roundWinnerObj.text;
+
+        if(roundWinnerObj.score === 5) {
+            const winnerDiv = document.querySelector('.winner');
+            winnerDiv.textContent = `${roundWinnerObj.player} wins!`;
+        }
+    })
+})
+
+
+
 const playGame = () => {
 
+    //remove logic that plays 5 rounods?
     let round = 0;
     for(let i = 0; i < 5; i++) {
        const text = playRound( getHumanChoice(), getComputerChoice() )
         alert(text);
         round++;
     }
+
     if (round === 5 && humanScore !== computerScore) {
         return humanScore > computerScore ? console.log('You win') : console.log('You Lose');
     } else {
@@ -47,4 +95,4 @@ const playGame = () => {
     }
 }
 
-playGame();
+// playGame();
